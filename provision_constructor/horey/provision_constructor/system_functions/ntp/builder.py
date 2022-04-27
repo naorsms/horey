71492734f2aa2692a2c354d1193ce5c6b2826753
horey <alexey.beley@gmail.com>
2022-04-27 18:12:30 +0300

    debug

--- provision_constructor/horey/provision_constructor/system_functions/ntp/builder.py
+++ provision_constructor/horey/provision_constructor/system_functions/ntp/builder.py
@@ -8,5 +8,5 @@ class Builder(SystemFunctionFactory.SystemFunction):
     def __init__(self, root_deployment_dir, provisioner_script_name, force=False):
         super().__init__(root_deployment_dir, provisioner_script_name, force=force)
         pdb.set_trace()
-        
+
 
